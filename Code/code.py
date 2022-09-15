0a859f51410dedcede28e4653291835fb18ef176
mengyao36 <mengyao.hu@stonybrook.edu>
2022-09-15 18:54:12 -0400

    add code

---
+++ Code/code.py
@@ -0,0 +1,13 @@
+### Statistics in Python https://scipy-lectures.org/packages/statistics/index.html#data-representation-and-interaction ###
+
+# import needed packages #
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+import seaborn as sns
